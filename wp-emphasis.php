504a9bda60a82f00a95d2a7f3464d49bdb8ac014
benbalter <benbalter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-12 17:58:40 +0000

    0.2

---
+++ wp-emphasis.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: WP Emphasis
+Plugin URI: http://ben.balter.com/2011/01/11/wordpress-emphasis-plugin/
+Description: WordPress implementation of NYT emphasis magic
+Version: 0.2
+Author: Benjamin J. Balter
+Author URI: http://ben.balter.com/
+License: GPL2
+*/
+
+function wp_emphasis_enqueue() {
+
+	// If SCRIPT_DEBUG is defined in config.php, use the dev (non-minified) version of the script (h/t @nacin)
+	$suffix = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '-src' : '';
+	
+	// Tell WordPress to queue the script for inclusion in the footer
+	// Also incudes the prototype framework if it is not already in the queue
+	wp_enqueue_script( 'wp_emphasis', plugins_url('/js/emphasis'.$suffix.'.js', __FILE__), array('prototype'), '1-11-11', 'true' );
+
+}
+
+add_action('wp_enqueue_scripts', 'wp_emphasis_enqueue');
+
+?>
