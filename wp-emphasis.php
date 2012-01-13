1b6bef57bf70dd90daec389f63a836ebfa313bac
benbalter <benbalter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-13 05:00:19 +0000

    readme

--- wp-emphasis.php
+++ wp-emphasis.php
@@ -15,36 +15,11 @@ function wp_emphasis_enqueue() {
 
 	// If SCRIPT_DEBUG is defined in config.php, use the dev (non-minified) version of the script (h/t @nacin)
 	$suffix = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '-src' : '';
-	
-	//lib and filename for jquery/prototype
-	$version = ( isset( $options['jquery'] ) && !$options['jquery'] ) ? '-prototype' : '';
-	$lib = ( isset($options['jquery']) && !$options['jquery'] ) ? array('prototype') : array('jquery');
-	
+		
 	// Tell WordPress to queue the script for inclusion in the footer
-	// Also incudes the prototype framework if it is not already in the queue
-	wp_enqueue_script( 'wp_emphasis', plugins_url('/js/emphasis'.$version.$suffix.'.js', __FILE__), $lib, filemtime(dirname(__FILE__).'/js/emphasis'.$version.$suffix.'.js'), 'true' );
+	// Also incudes the jQuery framework if it is not already in the queue
+	wp_enqueue_script( 'wp_emphasis', plugins_url('/js/emphasis'.$suffix.'.js', __FILE__), array('jquery'), filemtime(dirname(__FILE__).'/js/emphasis'.$suffix.'.js'), 'true' );
 
 }
 
 add_action('wp_enqueue_scripts', 'wp_emphasis_enqueue');
-
-function add_emphasis_option() {
-	register_setting(  'reading', 'wp_emphasis' );
-	add_settings_field( 'wp_emphasis_jquery', 'Emphasis Library', 'wp_emphasis_jquery_cb', 'reading');
-}
-
-add_action('admin_init','add_emphasis_option');
-
-	function wp_emphasis_jquery_cb() {
-		$options = get_option('wp_emphasis');
-		$fields = array(
-			'0' => 'Prototype',
-			'1' => 'jQuery'
-		);
-		foreach ( $fields as $field => $label ) {
-			echo '<label><input type="radio" name="wp_emphasis[jquery]" value="' . $field . '"' . checked( $options['jquery'], $field, false ) . '> ' . $label . '</label><br/>';
-		}
-	}
-
-
-?>
